35466b4d32597c92de74f9a491062b1fc9853710
Tzvi Friedman <tzvi.friedman@flatiron.com>
2021-10-25 01:20:57 -0400

    Fixed path creation and abstracted some vars

--- code.py
+++ code.py
@@ -10,22 +10,29 @@ import csv
 import os
 from csv import reader
 
-if not os.path.exists("Graduating Class 2034"):
-  os.mkdir("Graduating Class 2034")
-  for i in ['K','1','2','3','4']:
-    os.mkdir("Graduating Class 2034/" + i)
+grad_path_folder = "Graduating Class 2034"
+if not os.path.exists(grad_path_folder):
+  os.mkdir(grad_path_folder)
+  
 
-with open("KStudents2021.csv") as csvfile:
+with open("students.csv") as csvfile:
   csv_reader = reader(csvfile)
   header = next(csv_reader)
   for row in csv_reader:
     fullname = row[1] + row[0]
-    doc = Document("Template.docx")
+    fullname_path = row[0] + ", " + row[1]
+    # Make folder structure for each student
+    os.mkdir(grad_path_folder + "/" + fullname_path)
+    for i in ["K",'1','2','3','4']:
+      os.mkdir(grad_path_folder + "/" + fullname_path + "/" + i)
+    
+    ## Make the template
+    doc = Document("template.docx")
     replace_dict = {"first":row[1],"last":row[0]}
     for i in replace_dict:
       for p in doc.paragraphs:
         p.text=p.text.replace(i," " + replace_dict[i])
-    doc.save("Graduating Class 2034/K/" + fullname + ".docx")
+    doc.save("Graduating Class 2034/" + fullname_path + "/" + fullname_path + ".docx")
 
 
 
